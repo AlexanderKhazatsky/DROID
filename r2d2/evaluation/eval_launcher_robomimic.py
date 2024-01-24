7e785e6583ebc4808f35f200317b5939eb02ba67
suraj-nair-tri <suraj.nair@tri.global>
2024-01-23 18:22:05 -0800

    support for geting goal images during eval, removing need for controller

--- r2d2/evaluation/eval_launcher_robomimic.py
+++ r2d2/evaluation/eval_launcher_robomimic.py
@@ -15,6 +15,8 @@ import robomimic.utils.file_utils as FileUtils
 import robomimic.utils.torch_utils as TorchUtils
 import robomimic.utils.tensor_utils as TensorUtils
 
+import cv2
+
 def eval_launcher(variant, run_id, exp_id):
     # Get Directory #
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -36,9 +38,127 @@ def eval_launcher(variant, run_id, exp_id):
     device = TorchUtils.get_torch_device(try_to_use_cuda=True)
     ckpt_dict = FileUtils.maybe_dict_from_checkpoint(ckpt_path=ckpt_path)
     config = json.loads(ckpt_dict["config"])
+    print("CONFIG!")
 
     ### infer image size ###
-    imsize = ckpt_dict["shape_metadata"]["all_shapes"]["camera/image/hand_camera_left_image"][2]
+    imsize = 128 ## HARDCODE to 128 for now
+    # imsize = ckpt_dict["shape_metadata"]["all_shapes"]["camera/image/hand_camera_left_image"][0]
+    print("IMSIZE: ", imsize)
+
+    ckpt_dict["config"] = json.dumps(config)
+    policy, _ = FileUtils.policy_from_checkpoint(ckpt_dict=ckpt_dict, device=device, verbose=True)
+    policy.goal_mode = config["train"]["goal_mode"]
+
+    # determine the action space (relative or absolute)
+    action_keys = config["train"]["action_keys"]
+    if "action/rel_pos" in action_keys:
+        action_space = "cartesian_velocity"
+        for k in action_keys:
+            assert not k.startswith("action/abs_")
+    elif "action/abs_pos" in action_keys:
+        action_space = "cartesian_position"
+        for k in action_keys:
+            assert not k.startswith("action/rel_")
+    else:
+        raise ValueError
+
+    # determine the action space for the gripper
+    if "action/gripper_velocity" in action_keys:
+        gripper_action_space = "velocity"
+    elif "action/gripper_position" in action_keys:
+        gripper_action_space = "position"
+    else:
+        raise ValueError
+
+    # Prepare Policy Wrapper #
+    data_processing_kwargs = dict(
+        timestep_filtering_kwargs=dict(
+            action_space=action_space,
+            gripper_action_space=gripper_action_space,
+            robot_state_keys=["cartesian_position", "gripper_position", "joint_positions"],
+            # camera_extrinsics=[],
+        ),
+        image_transform_kwargs=dict(
+            remove_alpha=True,
+            bgr_to_rgb=True,
+            to_tensor=True,
+            augment=False,
+        ),
+    )
+    timestep_filtering_kwargs = data_processing_kwargs.get("timestep_filtering_kwargs", {})
+    image_transform_kwargs = data_processing_kwargs.get("image_transform_kwargs", {})
+
+    policy_data_processing_kwargs = {}
+    policy_timestep_filtering_kwargs = policy_data_processing_kwargs.get("timestep_filtering_kwargs", {})
+    policy_image_transform_kwargs = policy_data_processing_kwargs.get("image_transform_kwargs", {})
+
+    policy_timestep_filtering_kwargs.update(timestep_filtering_kwargs)
+    policy_image_transform_kwargs.update(image_transform_kwargs)
+
+    fs = config["train"]["frame_stack"]
+
+    wrapped_policy = PolicyWrapperRobomimic(
+        policy=policy,
+        timestep_filtering_kwargs=policy_timestep_filtering_kwargs,
+        image_transform_kwargs=policy_image_transform_kwargs,
+        frame_stack=fs,
+        eval_mode=True,
+    )
+
+    print("IMAGE TRANSFORM KWARGS: ", image_transform_kwargs)
+
+    camera_kwargs = dict(
+        hand_camera=dict(image=True, concatenate_images=False, resolution=(imsize, imsize), resize_func="cv2"),
+        varied_camera=dict(image=True, concatenate_images=False, resolution=(imsize, imsize), resize_func="cv2"),
+    )
+    
+    policy_camera_kwargs = {}
+    policy_camera_kwargs.update(camera_kwargs)
+
+    env = RobotEnv(
+        action_space=policy_timestep_filtering_kwargs["action_space"],
+        gripper_action_space=policy_timestep_filtering_kwargs["gripper_action_space"],
+        camera_kwargs=policy_camera_kwargs
+    )
+    # controller = VRPolicy()
+
+    # Launch GUI #
+    data_collector = DataCollecter(
+        env=env,
+        controller=None,
+        policy=wrapped_policy,
+        save_traj_dir=log_dir,
+        save_data=variant.get("save_data", True),
+    )
+    RobotGUI(robot=data_collector)
+
+
+def get_goal_im(variant, run_id, exp_id):
+    # Get Directory #
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # Prepare Log Directory #
+    variant["exp_name"] = os.path.join(variant["exp_name"], "run{0}/id{1}/".format(run_id, exp_id))
+    log_dir = os.path.join(dir_path, "../../evaluation_logs", variant["exp_name"])
+
+    # Set Random Seeds #
+    torch.manual_seed(variant["seed"])
+    np.random.seed(variant["seed"])
+
+    # Set Compute Mode #
+    use_gpu = variant.get("use_gpu", False)
+    torch.device("cuda:0" if use_gpu else "cpu")
+
+    ckpt_path = variant["ckpt_path"]
+
+    device = TorchUtils.get_torch_device(try_to_use_cuda=True)
+    ckpt_dict = FileUtils.maybe_dict_from_checkpoint(ckpt_path=ckpt_path)
+    config = json.loads(ckpt_dict["config"])
+
+    # import pdb; pdb.set_trace()
+    ### infer image size ###
+    imsize = 128
+    # imsize = ckpt_dict["shape_metadata"]["all_shapes"]["camera/image/hand_camera_left_image"][2]
 
     ckpt_dict["config"] = json.dumps(config)
     policy, _ = FileUtils.policy_from_checkpoint(ckpt_dict=ckpt_dict, device=device, verbose=True)
@@ -108,16 +228,13 @@ def eval_launcher(variant, run_id, exp_id):
     env = RobotEnv(
         action_space=policy_timestep_filtering_kwargs["action_space"],
         gripper_action_space=policy_timestep_filtering_kwargs["gripper_action_space"],
-        camera_kwargs=policy_camera_kwargs
+        camera_kwargs=policy_camera_kwargs,
+        do_reset=False
     )
-    controller = VRPolicy()
-
-    # Launch GUI #
-    data_collector = DataCollecter(
-        env=env,
-        controller=controller,
-        policy=wrapped_policy,
-        save_traj_dir=log_dir,
-        save_data=variant.get("save_data", True),
-    )
-    RobotGUI(robot=data_collector)
+    # import pdb; pdb.set_trace()
+    ims = env.read_cameras()[0]["image"]
+    for k in ims.keys():
+        image = ims[k]
+        cv2.imwrite(f'/home/ashwinbalakrishna/Desktop/r2d2-eval/goalims/{k}.png', image[:, :, :3])
+    return ims
+# '/home/ashwinbalakrishna/Desktop/r2d2-eval/goalims/a.png'
