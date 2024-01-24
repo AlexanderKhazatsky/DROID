3cbf4176741eb66ddfa8795b84b256bf5a1be7f0
suraj-nair-tri <suraj.nair@tri.global>
2024-01-23 18:23:28 -0800

    maybe reset (for goal im collection)

--- r2d2/robot_env.py
+++ r2d2/robot_env.py
@@ -13,7 +13,7 @@ from r2d2.misc.transformations import change_pose_frame
 
 
 class RobotEnv(gym.Env):
-    def __init__(self, action_space="cartesian_velocity", gripper_action_space=None, camera_kwargs={}):
+    def __init__(self, action_space="cartesian_velocity", gripper_action_space=None, camera_kwargs={}, do_reset=True):
         # Initialize Gym Environment
         super().__init__()
 
@@ -43,7 +43,8 @@ class RobotEnv(gym.Env):
         self.camera_type_dict = camera_type_dict
 
         # Reset Robot
-        self.reset()
+        if do_reset:
+            self.reset()
 
     def step(self, action):
         # Check Action
